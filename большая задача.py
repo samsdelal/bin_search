a193be6f8d800f1c8f93c5322a3a7923ff1ea0ad
samsdelal <55372911+samsdelal@users.noreply.github.com>
2020-03-14 15:13:19 +0700

    Update большая задача.py
    add timer

--- большая задача.py
+++ большая задача.py
@@ -1,5 +1,5 @@
-#Developer: Boris Kuznecov 19704.2 group
-#Algorith developed by Joseph Stein in 196
+# Developer: Boris Kuznecov 19704.2 group
+# Algorith developed by Joseph Stein in 196
 
 import random
 import time
@@ -7,13 +7,14 @@ import time
 
 def generate_list():
     z = []
-    for i in range(10):
+    for i in range(1000):
         z.append(random.randint(0, 10))
     # print(z)
     return z
 
 
 def simple_search(find_number, listi):
+    startt = time.time()
     mid_num = len(listi) // 2
     start = 0
     end = len(listi) - 1
@@ -24,20 +25,24 @@ def simple_search(find_number, listi):
             end = mid_num - 1
         mid_num = (start + end) // 2
     if start > end:
+        print('simple search - ', time.time() - startt)
         return None
+    print('simple search - ', time.time() - startt)
     return mid_num
 
 
-def recurse_search(find_number, listi):
+def recurse_search(find_number, listi, time_f):
     mid_num = len(listi) // 2
     if len(listi) == 0:
+        print('rec search - ', time.time() - time_f)
         return 'Нет такого числа'
     if listi[mid_num] == find_number:
+        print('rec search - ', time.time() - time_f)
         return mid_num
     else:
         if listi[mid_num] > find_number:
-            return recurse_search(find_number, listi[:mid_num])
-        return recurse_search(find_number, listi[mid_num + 1:])
+            return recurse_search(find_number, listi[:mid_num], time_f)
+        return recurse_search(find_number, listi[mid_num + 1:], time_f)
 
 
 def main():
@@ -46,9 +51,10 @@ def main():
     find_numberr = int(input('Введите искомое число - '))
     ist_of_numbers = list(generate_list())
     find_number = find_numberr
-    print(recurse_search(find_number, listq))
+    rec_start = time.time()
+    print(recurse_search(find_number, listq, rec_start))
     print(simple_search(find_number, listq))
-    print(f'Время работы программы - {time.time()-start}')
+    print(time.time() - start)
 
 
 main()
